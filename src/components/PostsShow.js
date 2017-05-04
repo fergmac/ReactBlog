ac10d503ac00075ce21f64742c2832c985c6dede
fergmac <f.macconnell@gmail.com>
2017-05-04 10:11:11 -0700

    display post data in PostsShow component

---
+++ src/components/PostsShow.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
+
+class PostsShow extends Component {
+    componentDidMount() {
+        // provided by react-router 
+        // params lists wild card tokens from the url { :id } from route
+        const { id } = this.props.match.params;
+        this.props.fetchPost(id);
+    }
+    render() {
+        const { post } = this.props;
+        // check to see if there is a post so we dont get post is undefined
+        if (!post) {
+            return <div>Loading...</div>;
+        }
+        return (
+            <div>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
+        )
+    }
+}
+// ownProps is the props object that is going to PostShow === this.props in render
+function mapStateToProps({ posts }, ownProps) {
+    return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
