bf853ba9410f2e6378c357dc92d8d1606beaf719
fergmac <f.macconnell@gmail.com>
2017-05-04 09:29:35 -0700

    create fetchPost action and reducer

--- src/reducers/reducer-posts.js
+++ src/reducers/reducer-posts.js
@@ -1,10 +1,20 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
+
 import _ from 'lodash';
 
 export default function (state = {}, action) {
     switch (action.type) {
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
+        case FETCH_POST:
+            // es5
+            // const post = action.payload.data;
+            // const newState = { ...state,  };
+            // newState[post.id] = post;
+            // return newState;
+
+            //es6 key interpolation: make new key on object using first property and give it value of second property
+            return { ...state, [action.payload.data.id]: action.payload.data };
         default:
             return state;
     }
